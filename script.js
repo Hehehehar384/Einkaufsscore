5464c7871ef6ca0b9acafcc8bdbb6a0d0a0bed54
Hehehehar384 <132939788+Hehehehar384@users.noreply.github.com>
2023-05-08 20:55:02 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,30 @@
-window.onload = function() {
-  alert("Welcome to my website!");
+// Initialize scores
+let scores = {
+  Jonas: 0,
+  Peter: 0,
+  Mama: 0,
+  Papa: 0,
+};
+
+// Update the score of a player
+function updateScore(playerName, score) {
+  scores[playerName] += score;
+  document.getElementById(`${playerName}-score`).innerHTML = scores[playerName];
 }
+
+// Add click event listeners to the buttons
+document.getElementById("jonas-add").addEventListener("click", function() {
+  updateScore("Jonas", 1);
+});
+
+document.getElementById("peter-add").addEventListener("click", function() {
+  updateScore("Peter", 1);
+});
+
+document.getElementById("mama-add").addEventListener("click", function() {
+  updateScore("Mama", 1);
+});
+
+document.getElementById("papa-add").addEventListener("click", function() {
+  updateScore("Papa", 1);
+});
