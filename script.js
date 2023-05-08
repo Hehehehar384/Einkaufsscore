4159521230c5e795b077f92ced746fcc626af734
Hehehehar384 <132939788+Hehehehar384@users.noreply.github.com>
2023-05-08 22:05:30 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,34 +1,78 @@
-$(document).ready(function() {
-  $('.plus-btn').click(function() {
-    var player = $(this).attr('id').split('-')[0];
-    var score = parseInt($('#'+player+'-score').text()) + 1;
-    $('#'+player+'-score').text(score);
+let scores;
+
+function loadScores() {
+  const xhr = new XMLHttpRequest();
+  xhr.open("GET", "/scores", false);
+  xhr.send();
+  scores = JSON.parse(xhr.responseText);
+}
+
+function saveScores() {
+  const xhr = new XMLHttpRequest();
+  xhr.open("POST", "/scores", true);
+  xhr.setRequestHeader("Content-type", "application/json");
+  xhr.onreadystatechange = function () {
+    if (xhr.readyState === XMLHttpRequest.DONE && xhr.status === 200) {
+      console.log(xhr.responseText);
+    }
+  };
+  xhr.send(JSON.stringify(scores));
+}
+
+function updateScore(player, amount) {
+  scores[player] += amount;
+  const scoreEl = document.getElementById(player);
+  scoreEl.textContent = scores[player];
+}
+
+window.onload = function () {
+  loadScores();
+  const jonasPlus = document.getElementById("jonas-plus");
+  const jonasMinus = document.getElementById("jonas-minus");
+  const peterPlus = document.getElementById("peter-plus");
+  const peterMinus = document.getElementById("peter-minus");
+  const mamaPlus = document.getElementById("mama-plus");
+  const mamaMinus = document.getElementById("mama-minus");
+  const papaPlus = document.getElementById("papa-plus");
+  const papaMinus = document.getElementById("papa-minus");
+
+  jonasPlus.addEventListener("click", function () {
+    updateScore("jonas", 1);
+    saveScores();
   });
 
-  $('.minus-btn').click(function() {
-    var player = $(this).attr('id').split('-')[0];
-    var score = parseInt($('#'+player+'-score').text()) - 1;
-    $('#'+player+'-score').text(score);
+  jonasMinus.addEventListener("click", function () {
+    updateScore("jonas", -1);
+    saveScores();
   });
 
-  $('#save-btn').click(function() {
-    var scores = {};
-    scores["Jonas"] = parseInt($('#jonas-score').text());
-    scores["Peter"] = parseInt($('#peter-score').text());
-    scores["Mama"] = parseInt($('#mama-score').text());
-    scores["Papa"] = parseInt($('#papa-score').text());
-    
-    $.ajax({
-      url: "/save-scores",
-      method: "POST",
-      contentType: "application/json",
-      data: JSON.stringify(scores),
-      success: function(response) {
-        console.log(response);
-      },
-      error: function(jqXHR, textStatus, errorThrown) {
-        console.log(textStatus, errorThrown);
-      }
-    });
+  peterPlus.addEventListener("click", function () {
+    updateScore("peter", 1);
+    saveScores();
   });
-});
+
+  peterMinus.addEventListener("click", function () {
+    updateScore("peter", -1);
+    saveScores();
+  });
+
+  mamaPlus.addEventListener("click", function () {
+    updateScore("mama", 1);
+    saveScores();
+  });
+
+  mamaMinus.addEventListener("click", function () {
+    updateScore("mama", -1);
+    saveScores();
+  });
+
+  papaPlus.addEventListener("click", function () {
+    updateScore("papa", 1);
+    saveScores();
+  });
+
+  papaMinus.addEventListener("click", function () {
+    updateScore("papa", -1);
+    saveScores();
+  });
+};
