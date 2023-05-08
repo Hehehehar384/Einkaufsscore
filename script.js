cf9318959e629fb0b306cdc6db88f082f5aa23d6
Hehehehar384 <132939788+Hehehehar384@users.noreply.github.com>
2023-05-08 22:01:12 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,65 +1,34 @@
-// Initialize the scores
-let scores = [0, 0, 0, 0];
+$(document).ready(function() {
+  $('.plus-btn').click(function() {
+    var player = $(this).attr('id').split('-')[0];
+    var score = parseInt($('#'+player+'-score').text()) + 1;
+    $('#'+player+'-score').text(score);
+  });
 
-// Update the scores in the HTML table
-function updateScores() {
-    for (let i = 1; i <= 4; i++) {
-        document.getElementById("score" + i).innerHTML = scores[i - 1];
-    }
-}
+  $('.minus-btn').click(function() {
+    var player = $(this).attr('id').split('-')[0];
+    var score = parseInt($('#'+player+'-score').text()) - 1;
+    $('#'+player+'-score').text(score);
+  });
 
-// Add a point to the score of the given player
-function addScore(player) {
-    scores[player - 1]++;
-    updateScores();
-}
-
-// Subtract a point from the score of the given player
-function subScore(player) {
-    scores[player - 1]--;
-    updateScores();
-}
-
-// Send the scores to the server to be saved
-function saveScores() {
-    fetch('/saveScores', {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json'
-        },
-        body: JSON.stringify({ scores })
-    })
-    .then(response => {
+  $('#save-btn').click(function() {
+    var scores = {};
+    scores["Jonas"] = parseInt($('#jonas-score').text());
+    scores["Peter"] = parseInt($('#peter-score').text());
+    scores["Mama"] = parseInt($('#mama-score').text());
+    scores["Papa"] = parseInt($('#papa-score').text());
+    
+    $.ajax({
+      url: "/save-scores",
+      method: "POST",
+      contentType: "application/json",
+      data: JSON.stringify(scores),
+      success: function(response) {
         console.log(response);
-    })
-    .catch(error => {
-        console.error(error);
+      },
+      error: function(jqXHR, textStatus, errorThrown) {
+        console.log(textStatus, errorThrown);
+      }
     });
-}
-
-// Load the saved scores from the server
-function loadScores() {
-    fetch('/loadScores')
-    .then(response => {
-        return response.json();
-    })
-    .then(data => {
-        scores = data.scores;
-        updateScores();
-    })
-    .catch(error => {
-        console.error(error);
-    });
-}
-
-// Load the saved scores when the page is loaded
-window.onload = function() {
-    loadScores();
-}
-
-// Save the scores when the page is unloaded
-window.onbeforeunload = function() {
-    saveScores();
-}
-
-
+  });
+});
