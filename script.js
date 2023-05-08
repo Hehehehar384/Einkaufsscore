d4db24210a4a7e5667c9bb43ab7576cea4fd2569
Hehehehar384 <132939788+Hehehehar384@users.noreply.github.com>
2023-05-08 21:24:53 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,29 +1,52 @@
-// Define the updateScore function
-function updateScore(scoreId) {
-  var scoreElement = document.getElementById(scoreId);
-  var currentScore = parseInt(scoreElement.innerHTML);
-  scoreElement.innerHTML = currentScore + 1;
+let scores = { "Jonas": 0, "Peter": 0, "Mama": 0, "Papa": 0 };
+
+// Load the scores from local storage if they exist
+if (localStorage.getItem("scores")) {
+  scores = JSON.parse(localStorage.getItem("scores"));
+  updateScoreboard();
 }
 
-// Get the buttons
-var jonasButton = document.getElementById("jonas-add");
-var peterButton = document.getElementById("peter-add");
-var mamaButton = document.getElementById("mama-add");
-var papaButton = document.getElementById("papa-add");
-
-// Add click event listeners to the buttons
-jonasButton.onclick = function() {
-  updateScore("Jonas-score");
+// Update the score of a player
+function updateScore(player, amount) {
+  scores[player] += amount;
+  updateScoreboard();
+  saveScores();
 }
 
-peterButton.onclick = function() {
-  updateScore("Peter-score");
+// Update the scoreboard with the current scores
+function updateScoreboard() {
+  let scoreboard = document.getElementById("scoreboard");
+  let rows = scoreboard.getElementsByTagName("tr");
+
+  for (let i = 0; i < rows.length; i++) {
+    let player = rows[i].getElementsByTagName("td")[0].textContent;
+    let score = scores[player];
+    rows[i].getElementsByTagName("td")[1].textContent = score;
+  }
 }
 
-mamaButton.onclick = function() {
-  updateScore("Mama-score");
+// Save the scores to local storage
+function saveScores() {
+  localStorage.setItem("scores", JSON.stringify(scores));
 }
 
-papaButton.onclick = function() {
-  updateScore("Papa-score");
+// Add event listeners to the buttons
+let buttons = document.getElementsByTagName("button");
+for (let i = 0; i < buttons.length; i++) {
+  buttons[i].addEventListener("click", function() {
+    let player = this.parentNode.getElementsByTagName("td")[0].textContent;
+    let amount = parseInt(this.getAttribute("data-value"));
+    updateScore(player, amount);
+  });
 }
+
+// Add event listeners to the minus buttons
+let minusButtons = document.getElementsByClassName("minus");
+for (let i = 0; i < minusButtons.length; i++) {
+  minusButtons[i].addEventListener("click", function() {
+    let player = this.parentNode.getElementsByTagName("td")[0].textContent;
+    let amount = parseInt(this.getAttribute("data-value"));
+    updateScore(player, amount);
+  });
+}
+
